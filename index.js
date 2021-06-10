bea0d5fe00ca8d0cc31ac896bad99e375b5b4d2b
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2021-06-10 14:09:02 -0700

    feat: handle updates to lock file

--- index.js
+++ index.js
@@ -1,75 +1,49 @@
-import { App, Octokit } from "octokit";
-import dotenv from "dotenv";
+const execa = require("execa");
 
-dotenv.config();
+run();
 
-const app = new App({
-  appId: process.env.APP_ID,
-  privateKey: process.env.PRIVATE_KEY,
-});
-
-const { data: appInfo } = await app.octokit.request("GET /app");
-console.log("Authenticated as %s app", appInfo.slug);
-
-app.eachRepository(async ({ octokit, repository }) => {
-  console.log("Sending requests to %s", repository.full_name);
-
-  const randomName = "test-" + Date.now();
-
-  // https://developer.github.com/v3/repos/commits/#list-commits-on-a-repository
-  const {
-    data: [latestCommit],
-  } = await octokit.rest.repos.listCommits({
-    owner: repository.owner.login,
-    repo: repository.name,
-    sha: repository.default_branch,
-    per_page: 1,
-  });
-
-  await octokit.rest.git.createRef({
-    owner: repository.owner.login,
-    repo: repository.name,
-    sha: latestCommit.sha,
-    ref: `refs/heads/${randomName}`,
-  });
-
-  console.log("%s branch created", randomName);
-
-  const { data: newFile } = await octokit.rest.repos.createOrUpdateFileContents(
-    {
-      owner: repository.owner.login,
-      repo: repository.name,
-      path: `${randomName}.txt`,
-      content: Buffer.from("hello world").toString("base64"),
-      message: "creating test file",
-      branch: randomName,
+async function run() {
+  try {
+    await command("git checkout update-lockfile");
+  } catch (error) {
+    if (
+      !/pathspec 'update-lockfile' did not match any file/.test(error.stderr)
+    ) {
+      throw error;
     }
-  );
 
-  console.log("%s created via %s", randomName, newFile.commit.html_url);
+    await command("git checkout -b update-lockfile");
+  }
 
-  // NOTE: this will fail due to branch protection
-  // await octokit.rest.repos.merge({
-  //   owner: repository.owner.login,
-  //   repo: repository.name,
-  //   base: repository.default_branch,
-  //   head: randomName,
-  //   head: "test-1622752252981",
-  // });
+  await command("rm -rf package-lock.json node_modules");
+  await command("npm install");
 
-  // NOTE: need to authenticate as a repository admin
-  const gr2mOctokit = new Octokit({
-    auth: process.env.GITHUB_TOKEN,
+  const result = await command("git diff --name-only");
+
+  if (!/package-lock\.json/.test(result.all)) {
+    console.log("no changes.");
+    process.exit(0);
+  }
+
+  await command("git commit package-lock.json -m", [
+    "build(package): lock file update",
+  ]);
+
+  await command("git push origin update-lockfile");
+
+  console.log("done.");
+}
+
+async function command(cmd, extraCmdOptions = []) {
+  const [bin, ...cmdOptions] = cmd.split(/\s+/);
+
+  console.log("$ %s", cmd);
+
+  const result = await execa(bin, [...cmdOptions, ...extraCmdOptions], {
+    all: true,
   });
 
-  await gr2mOctokit.rest.repos.merge({
-    owner: repository.owner.login,
-    repo: repository.name,
-    base: repository.default_branch,
-    head: randomName,
-    // disable retries in case of 409 error
-    request: { retries: 0 },
-  });
+  console.log(result.all);
 
-  console.log("Branch merged");
-});
+  return result;
+}
